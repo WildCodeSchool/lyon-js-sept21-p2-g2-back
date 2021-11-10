b644a7495faf8cafa2843640a62cdc524ef18251
lea-weibel <lea.weibel00@gmail.com>
2021-11-10 10:23:23 +0100

    Add get for destination page et blog posts

--- app.js
+++ app.js
@@ -10,17 +10,23 @@ const blogPosts = [
     name: 'Nicolas C.',
     avatar:
       'https://images.unsplash.com/photo-1623330188314-8f4645626731?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=417&q=80https://unsplash.com/photos/-Tc8w2Kvsf8',
-    date: { beginning: '12/06/2020', end: '21/06/2020' },
-    message: 'blablabla',
+    date: { beginning: '12.06.2020', end: '21.06.2020' },
+    message:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
     tags: ['food', 'thrills', 'hiking'],
-    photos: [],
+    photos: [
+      'https://images.unsplash.com/photo-1523592121529-f6dde35f079e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+
+      'https://images.unsplash.com/photo-1501179691627-eeaa65ea017c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      'https://images.unsplash.com/photo-1526494631344-8c6fa6462b17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+    ],
     country: 'indonesia',
   },
   {
     id: 2,
     name: 'Erembert Q.',
     avatar: 'https://unsplash.com/photos/6anudmpILw4',
-    date: { beginning: '24/02/2019', end: '22/03/2019' },
+    date: { beginning: '24.02.2019', end: '22.03.2019' },
     message: 'blablablablabla',
     tags: ['architecture', 'museum', 'history', 'culture'],
     photos: [],
@@ -31,7 +37,7 @@ const blogPosts = [
     name: 'Joshua B.',
     avatar:
       'https://images.unsplash.com/photo-1624561172888-ac93c696e10c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=389&q=80',
-    date: { beginning: '02/09/2021', end: '16/09/2021' },
+    date: { beginning: '02.09.2021', end: '16.09.2021' },
     message: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
     totam quis nam. Placeat modi error non eveniet eligendi expedita autem
     officia? Aperiam soluta, iste sequi, perferendis aliquam molestiae
@@ -40,7 +46,12 @@ const blogPosts = [
     quaerat nam rem, a nihil ex? Pariatur in ullam delectus eius. Ad, illum
     neque.`,
     tags: ['music', 'religion', 'culture'],
-    photos: [],
+    photos: [
+      'https://images.unsplash.com/photo-1480796927426-f609979314bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+      'https://images.unsplash.com/photo-1528164344705-47542687000d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=892&q=80',
+      'https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+      'https://images.unsplash.com/photo-1533050487297-09b450131914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    ],
     country: 'japan',
   },
 ];
@@ -51,7 +62,11 @@ app.get('/destinations/:destination/blog-posts', (req, res) => {
   res.send(blogPosts.filter((post) => post.country === destination));
 });
 
-app.get('/blog-posts/:id');
+app.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  res.send(blogPosts.filter((post) => post.id === parseInt(id)));
+});
 
 app.post('/destinations/:destination/blog-posts');
 
