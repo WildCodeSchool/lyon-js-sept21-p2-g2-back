104ff44cda9b8d2b3ba222215d40e954e5845d68
lea-weibel <lea.weibel00@gmail.com>
2021-11-16 10:11:43 +0100

    stash

--- app.js
+++ app.js
@@ -94,8 +94,18 @@ const blogPosts = [
 
 app.get('/destinations/:destination/blog-posts', (req, res) => {
   const { destination } = req.params;
-  console.log(destination);
-  res.send(blogPosts.filter((post) => post.country === destination));
+  // console.log(destination);
+  connection.query(
+    'SELECT * FROM post WHERE country=destination',
+    (err, results) => {
+      if (err) {
+        res.status(500).send(`An error occurred: ${err.message}`);
+      } else {
+        res.status(200).send(results);
+      }
+    }
+  );
+  // res.send(blogPosts.filter((post) => post.country === destination));
 });
 
 app.get('/posts/:id', (req, res) => {
